7a37b2dae763d8aa979b2d7fc44337f5822377c1
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2024-03-01 08:06:16 +0300

    useState Hook

---
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -0,0 +1,33 @@
+import { data } from "../../../data"
+import React from 'react'
+
+const UseStateArray = () => {
+  const [people, setPeople] = React.useState(data )
+
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+  const clearAllItems = () => {
+    setPeople([])
+  }
+
+  return <div>
+    {
+      people.map((person) =>{
+        const {id,name} = person
+        console.log(person);
+        return (<div key={id}>
+          <h4>{name}</h4>
+          <button type='button' onClick={() =>removeItem(id)}>remove</button>
+        </div>
+        )
+
+    })}
+    <button type='button' style={{marginTop:'2rem'}}
+    className="btn" onClick={clearAllItems}
+    >clear items</button>
+  </div>
+};
+
+export default UseStateArray;

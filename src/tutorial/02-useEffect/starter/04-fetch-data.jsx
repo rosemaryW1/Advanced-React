5b02bef011c4bb6a9ee1dbe4cb4e75c6c89c34b3
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2024-03-01 16:41:09 +0300

    Fetch Api

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,28 @@
+import { useState,useEffect } from "react";
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
+  const [users,setUsers] = useState([])
+
+  useEffect(() => {
+    try{
+      const fetchData = async() =>{
+      const response = await fetch(url)
+      const users = await response.json()
+      console.log(users)
+    }
+  }
+    catch (error){
+        console.log(error)
+    }
+
+    
+    
+    fetchData()
+  },[])
+
+
   return <h2>fetch data example</h2>;
 };
 export default FetchData;
